4190823bcdb22e680b03463bb8ad088c15706938
King <47607185+DarkGamerYT@users.noreply.github.com>
2025-02-20 22:03:06 +0100

    New project!

---
+++ src/main/java/com/xkingdark/kingsveinminer/Main.java
@@ -0,0 +1,10 @@
+package com.xkingdark.kingsveinminer;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Main implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
